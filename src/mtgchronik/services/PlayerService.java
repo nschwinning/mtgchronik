087e6486005378a523d36d6da7eac0d7b29aafe0
nils <nils@DESKTOP-06T3PBE>
2017-02-23 19:10:20 +0100

    Changes from at home

--- src/mtgchronik/services/PlayerService.java
+++ src/mtgchronik/services/PlayerService.java
@@ -8,7 +8,9 @@ import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import mtgchronik.entities.LineUp;
 import mtgchronik.entities.Player;
+import mtgchronik.entities.PlayerInstance;
 
 @Stateless
 public class PlayerService {
@@ -27,6 +29,20 @@ public class PlayerService {
 		return player;
 	}
 	
+	public PlayerInstance createPlayerInstance(LineUp lineUp,int position){
+		PlayerInstance playerInstance = new PlayerInstance(lineUp, position);
+		em.persist(playerInstance);
+		return playerInstance;
+	}
+	
+	public PlayerInstance updatePlayerInstance(PlayerInstance playerInstance){
+		if (playerInstance.getId()==0){
+			em.persist(playerInstance);
+			return playerInstance;
+		}
+		return em.merge(playerInstance);
+	}
+	
 	public Player getPlayerByName(String firstName,String lastName){
 		TypedQuery<Player> q = em.createQuery("FROM Player WHERE firstName=:firstName AND lastName=:lastName", Player.class);
 		q.setParameter("firstName", firstName);
@@ -37,6 +53,12 @@ public class PlayerService {
 		catch (NoResultException e){
 			return null;
 		}
+	}
+	
+	public List<PlayerInstance> getPlayerInstancesForLineUp(LineUp lineUp){
+		TypedQuery<PlayerInstance> q = em.createQuery("FROM PlayerInstance WHERE lineUp=:lineUp", PlayerInstance.class);
+		q.setParameter("lineUp", lineUp);
+		return q.getResultList();
 	}
 
 }
--- src/mtgchronik/webfrontend/AdministrationTeamInstanceView.java
+++ src/mtgchronik/webfrontend/AdministrationTeamInstanceView.java
@@ -1,11 +1,18 @@
 package mtgchronik.webfrontend;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 import mtgchronik.entities.LineUp;
+import mtgchronik.entities.Player;
+import mtgchronik.entities.PlayerInstance;
 import mtgchronik.entities.TeamInstance;
+import mtgchronik.services.PlayerService;
 import mtgchronik.services.TeamService;
 
 @ManagedBean
@@ -15,13 +22,25 @@ public class AdministrationTeamInstanceView {
 	@Inject
 	private TeamService teamService;
 	
+	@Inject
+	private PlayerService playerService;
+	
 	private long teamInstanceId;
 	private TeamInstance teamInstance;
 	private LineUp lineUp;
+	private List<PlayerInstance> lineUpPlayers;
+	private List<Player> players;
 	
 	public void loadTeamInstance(){
 		this.teamInstance=teamService.getTeamInstanceByID(teamInstanceId);
 		this.setLineUp(teamService.getLineUpForTeamInstance(teamInstance));
+		if (lineUp!=null){
+			lineUpPlayers = playerService.getPlayerInstancesForLineUp(lineUp);
+		}
+		else {
+			lineUpPlayers = new ArrayList<PlayerInstance>();
+		}
+		players = playerService.getAllPlayers();
 	}
 
 	public long getTeamInstanceId() {
@@ -47,6 +66,14 @@ public class AdministrationTeamInstanceView {
 	
 	public void createLineUp(){
 		lineUp=teamService.createLineUp(teamInstance);
+		for (int i=1;i<=6;i++){
+			PlayerInstance playerInstance = playerService.createPlayerInstance(lineUp, i);
+			lineUpPlayers.add(playerInstance);
+		}
+	}
+	
+	public List<PlayerInstance> getLineUpPlayers(){
+		return lineUpPlayers;
 	}
 	
 }
